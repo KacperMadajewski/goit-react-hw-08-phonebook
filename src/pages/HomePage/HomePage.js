06bf1fa2cbfae8e3e87316e3bdf07c19d213b5c6
KacperMadajewski <kacpermadajewski@gmail.com>
2023-07-22 20:22:06 +0200

    some changes in styles

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import css from './HomePage.module.css';
 const HomePage = () => {
   return (
     <div className={css.header__box}>
-      <h1 className={css.header__title}>Your own Phonebook</h1>
+      <h1 className={css.header__title}>Your own Phonebook!</h1>
       <p>Please register or login</p>
     </div>
   );
